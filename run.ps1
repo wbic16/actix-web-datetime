8f6e305a24c6ae68d6b661c8fce10846a5fad9ef
Will Bickford <wbic16@gmail.com>
2024-01-28 12:41:02 -0600

    debugging

--- run.ps1
+++ run.ps1
@@ -1,15 +1,16 @@
 # apply cargo/trunk to get leptos running
 # to read more: https://book.leptos.dev/getting_started/index.html
 
-cargo build
-cargo test
-write-host "http://localhost:8000/ should be working now - press Ctrl-c to continue"
-cargo run
-
-
 cargo add leptos --features=csr,nightly --package actix-web-datetime-frontend
 rustup override set nightly
 rustup target add wasm32-unknown-unknown
-set-location frontend
+
+cargo build
+cargo test
+write-host "http://localhost:8000/ should be working now - press Ctrl-c to continue"
+$result = & cargo run
+
+push-location frontend
 trunk serve --open
 write-host "http://localhost:8080/ should be working now"
+pop-location
